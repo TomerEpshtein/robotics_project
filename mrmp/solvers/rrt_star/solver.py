d5406b3236bd50c8df8e6ff08a10167b196bcf2d
TomerEpshtein <a@a.com>
2022-02-05 12:55:24 +0200

    Change NEAREST_POINT_SEARCH

--- mrmp/solvers/rrt_star/solver.py
+++ mrmp/solvers/rrt_star/solver.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from bindings import *
 from geometry_utils import collision_detection
 from mrmp.conversions import to_point_2_list
+from mrmp.solvers.my_algo.prm_2_minlen import calculate_length_from_start_to_end
 from mrmp.solvers.rrt_star.nearest_point_search import NearestPointSearch
 from mrmp.solvers.rrt_star.random_points_in_polygon_generator import PointsGenerator
 from mrmp.solvers.rrt_star.rrt_star import RrtStar
@@ -12,11 +13,10 @@ STEP_SIZE = FT(Gmpq(STEP_SIZE_VALUE))
 NUM_SAMPLES = 1000
 
 
-def _get_total_distance(path, nn: NearestPointSearch):
-    counter = 0
-    for i in range(len(path) - 1):
-        counter = counter + nn.get_distance(path[i], path[i + 1]).to_double()
-    return counter
+def _get_total_distance(path, num_robots):
+    # divide the path to point_2_list in each element instead point_d
+    path_divided = [to_point_2_list(p, num_robots) for p in path]
+    return calculate_length_from_start_to_end(path_divided)
 
 
 def generate_path_disc(robots, obstacles, disc_obstacles, destinations, argument, writer, isRunning):
@@ -36,7 +36,7 @@ def generate_path_disc(robots, obstacles, disc_obstacles, destinations, argument
                            radii]
 
     center_points = [robot['center'] for robot in robots]
-    nearest_point_search = NearestPointSearch(num_robots)
+    nearest_point_search = NearestPointSearch([])
     points_generator = PointsGenerator(obstacles, num_robots, radii, collision_detectors)
     rrt = RrtStar(STEP_SIZE, points_generator, num_samples, nearest_point_search,
                   collision_detectors, num_robots, radii)
@@ -51,7 +51,7 @@ def generate_path_disc(robots, obstacles, disc_obstacles, destinations, argument
         if generated_path is not None:
             for p in generated_path:
                 path.append(to_point_2_list(p, num_robots))
-            print(_get_total_distance(generated_path, nearest_point_search), file=writer)
+            print(_get_total_distance(generated_path, num_robots), file=writer)
 
     print(f"total time {datetime.now() - start_time}", file=writer)
 
